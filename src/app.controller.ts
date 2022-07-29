951f5c5ff5d15eb3db71c8b03fffcb0795e006cb
Gabriel Felipe Bello <86967978+gabrielfbello@users.noreply.github.com>
2022-07-29 14:22:28 -0300

    Teste

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from "@nestjs/common";
 export class AppController {
   @Get()
   root(): string {
-    return "Deployed successfully 🚀";
+    return JSON.stringify(new Date());
   }
 }
